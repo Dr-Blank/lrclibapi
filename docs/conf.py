b6d78cac62bfb7ffb41effb475386d133fdda451
Dr-Blank <64108942+Dr-Blank@users.noreply.github.com>
2023-11-09 11:50:36 -0500

    Update project name and add new Sphinx extension.

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "lrclibapi"
+project = "LRCLib Python API"
 copyright = "2023, Dr-Blank"
 author = "Dr-Blank"
 release = "0.0.1"
@@ -18,6 +18,7 @@ extensions = [
     "sphinx.ext.autodoc",  # to generate the docs from the docstrings
     "sphinx.ext.todo",  # to document the todos
     "sphinx.ext.viewcode",  # to generate the source code
+    "sphinx.ext.autosectionlabel",  # to reference the sections with their titles
     "sphinx.ext.napoleon",  # to support the Google and NumPy docstrings
     "nbsphinx",  # to support the Jupyter notebooks
 ]
@@ -29,5 +30,5 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
