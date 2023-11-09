52439f86864886e533fc60c21407cd17004747bf
Dr-Blank <64108942+Dr-Blank@users.noreply.github.com>
2023-11-09 03:21:56 -0500

    generate docs using autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.viewcode']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
--- docs/index.rst
+++ docs/index.rst
@@ -10,11 +10,4 @@ Welcome to lrclib's documentation!
    :maxdepth: 2
    :caption: Contents:
 
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+   modules
