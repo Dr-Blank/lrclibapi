88f3141eb0fc44c2188c039702e9f6c91cfb14cd
Dr-Blank <64108942+Dr-Blank@users.noreply.github.com>
2023-11-09 09:53:13 -0500

    Update Sphinx configuration The commit updates the Sphinx configuration by adding new extensions and changing the HTML theme.

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'lrclibapi'
-copyright = '2023, Dr-Blank'
-author = 'Dr-Blank'
-release = '0.0.1'
+project = "lrclibapi"
+copyright = "2023, Dr-Blank"
+author = "Dr-Blank"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.viewcode']
+extensions = [
+    "sphinx.ext.autodoc",  # to generate the docs from the docstrings
+    "sphinx.ext.todo",  # to document the todos
+    "sphinx.ext.viewcode",  # to generate the source code
+    "sphinx.ext.napoleon",  # to support the Google and NumPy docstrings
+    "nbsphinx",  # to support the Jupyter notebooks
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "furo"
+html_static_path = ["_static"]
